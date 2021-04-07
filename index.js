79fef41165107505cd9677f497c87fdc89f1a2c0
bolub <abiol5202@gmail.com>
2021-04-07 19:37:26 +0100

    javascript variables

--- index.js
+++ index.js
@@ -1,3 +1,77 @@
-function showAlert() {
-  alert("This is an alert");
-}
+// If you want to learn how comments work, visit this link, https://www.w3schools.com/js/js_comments.asp
+
+let variableA = "bolu";
+
+let variableB = 5;
+
+let variableC = true;
+
+// array varibale
+let varaiableD = ["hello", "hi", 1, 2, 3];
+
+// object variable
+let variableE = {
+  name: "Bolu",
+  age: 45,
+  occupation: "Software developer",
+};
+
+// ================== ARRAYS =====================================
+let newArray = ["item1", "item2", "item3", "item4", "item5"];
+
+// to get the length of an array
+// console.log(newArray.length);
+
+// how to always get the last item of an array
+let lengthOfArray = newArray.length;
+console.log(newArray[lengthOfArray - 1]);
+
+// =============================================================================
+
+// ================== MATHEMATICL OPERATIONS ====================================
+
+let a = 5;
+let b = 10;
+
+let total = 5 + 10;
+
+// this displays the total of the two numbers
+// console.log(total);
+
+// this concatenates the string and the total
+// console.log("The total = " + total);
+
+// =============================================================================
+
+// ================== COMPARISON OPERATIONS ====================================
+
+let opt1 = 40;
+let opt2 = 100;
+
+// regular if statement
+// if (opt1 > opt2) {
+//   alert("Opt1 is greater than Opt2");
+// }
+
+// if and else statement
+// if (opt1 > opt2) {
+//   alert("Opt1 is greater than Opt2");
+// } else {
+//   alert("Opt1 is less than Opt2");
+// }
+
+// switch statement
+// switch (opt1) {
+//   case 50:
+//     alert("Opt 1 is equal to 50");
+//     break;
+
+//   case 100:
+//     alert("Opt 1 is equal to 100");
+//     break;
+
+//   default:
+//     alert("No options are satisfied");
+// }
+
+// ==============================================================================
